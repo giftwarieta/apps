7d9990ea1c574c8b0908a3f727cacba8f6672ae6
Warieta Gift Ejovwoke <63240882+giftwarieta@users.noreply.github.com>
2024-06-14 14:10:19 +0100

    Update uber_pickups.py

--- uber_pickups.py
+++ uber_pickups.py
@@ -32,3 +32,8 @@ hist_values = np.histogram(
     data[DATE_COLUMN].dt.hour, bins=24, range=(0,24))[0]
 
 st.bar_chart(hist_values)
+
+# Plot data on a map
+st.subheader('Map of all pickups')
+st.map(data)
+
