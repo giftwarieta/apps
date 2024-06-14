0602b7992504ffd6d9725dee77890a9845205bed
Warieta Gift Ejovwoke <63240882+giftwarieta@users.noreply.github.com>
2024-06-14 14:41:39 +0100

    Update uber_pickups.py

--- uber_pickups.py
+++ uber_pickups.py
@@ -37,11 +37,14 @@ st.bar_chart(hist_values)
 st.subheader('Map of all pickups')
 st.map(data)
 
-hour_to_filter = 17
+#Adding filters
+hour_to_filter = st.slider('hour', 0, 23, 17)  # min: 0h, max: 23h, default: 17h
+#hour_to_filter = 17
+
+# display data
 filtered_data = data[data[DATE_COLUMN].dt.hour == hour_to_filter]
 st.subheader(f'Map of all pickups at {hour_to_filter}:00')
 st.map(filtered_data)
 
-#Adding filters
-hour_to_filter = st.slider('hour', 0, 23, 17)  # min: 0h, max: 23h, default: 17h
+
 
