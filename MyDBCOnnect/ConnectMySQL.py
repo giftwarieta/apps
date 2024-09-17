f0920ab307b41e015dd20384cd666519146a4bfc
Warieta Gift Ejovwoke <63240882+giftwarieta@users.noreply.github.com>
2024-09-17 17:23:10 +0100

    Update ConnectMySQL.py

--- MyDBCOnnect/ConnectMySQL.py
+++ MyDBCOnnect/ConnectMySQL.py
@@ -6,5 +6,17 @@ Created on Wed Sep 11 14:22:27 2024
 
 import streamlit as st
 import pandas as pd
+import mysql.connector
+import os
+from pathlib import Path
+from dotenv import load_dotenv
 
-st.write('Welcome home')
+# configuring time
+from datetime import datetime
+from datetime import date
+from datetime import timedelta
+
+today = date.today()
+thisday = today.strftime('%A, %B %d %Y')
+
+st.write('This script ran today, ' + thisday)
