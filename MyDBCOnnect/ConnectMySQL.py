76f5f6e922c340cbf498e700afcdfe869fb4c77b
Warieta Gift Ejovwoke <63240882+giftwarieta@users.noreply.github.com>
2024-09-17 17:20:39 +0100

    Update ConnectMySQL.py

--- MyDBCOnnect/ConnectMySQL.py
+++ MyDBCOnnect/ConnectMySQL.py
@@ -6,20 +6,5 @@ Created on Wed Sep 11 14:22:27 2024
 
 import streamlit as st
 import pandas as pd
-import mysql.connector
-import os
-from pathlib import Path
-from dotenv import load_dotenv
 
-# configuring time
-from datetime import datetime
-from datetime import date
-from datetime import timedelta
-
-
-
-st.write('I am doing great')
-
-#df = pd.DataFrame(data, columns=cursor.column_names)
-
-#st.write(df)
+st.write('Welcome home')
