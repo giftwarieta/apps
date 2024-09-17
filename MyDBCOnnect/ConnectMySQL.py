eb1c9c8b01b1c9ba939ab7f61b805302b7a56bca
Warieta Gift Ejovwoke <63240882+giftwarieta@users.noreply.github.com>
2024-09-17 17:19:38 +0100

    Update ConnectMySQL.py

--- MyDBCOnnect/ConnectMySQL.py
+++ MyDBCOnnect/ConnectMySQL.py
@@ -1,5 +1,3 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
 """
 Created on Wed Sep 11 14:22:27 2024
 
@@ -18,41 +16,6 @@ from datetime import datetime
 from datetime import date
 from datetime import timedelta
 
-today = date.today()
-thisday = today.strftime('%A, %B %d %Y')
-
-print('This script ran today, ' + thisday)
-
-
-env_path = Path.cwd() / '.evn'
-
-current_dir = Path(_file_).resolve().parent if "_file_" in locals() else Path.cwd()
-envars = current_dir / ".env"
-load_dotenv(envars)
-load_dotenv(dotenv_path = env_path)
-
-
-db = mysql.connector.connect(
-        host = os.environ['host'],
-        user = os.environ['user'],
-        password = os.environ['password'],
-        database = os.environ['database']
-)
-
-cursor = db.cursor()
-
-
-#cursor.execute("show databases")
-
-
-#for x in cursor:
-#	print(x)
-
-data = cursor.execute("Select * from users")
-
-#print(cursor.fetchall())
-
-print(cursor.fetchone())
 
 
 st.write('I am doing great')
