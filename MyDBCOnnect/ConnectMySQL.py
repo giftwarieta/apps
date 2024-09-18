eade3c943423068d11c87564ec1313a5f4b7ec33
Warieta Gift Ejovwoke <63240882+giftwarieta@users.noreply.github.com>
2024-09-18 10:31:11 +0100

    Update ConnectMySQL.py

--- MyDBCOnnect/ConnectMySQL.py
+++ MyDBCOnnect/ConnectMySQL.py
@@ -86,12 +86,7 @@ def visacard_image():
         """
 
 if title == 'Yes':
-    st.markdown(
-    """
-    <svg height="100" width="100">
-      <circle cx="50" cy="50" r="40" stroke="black" stroke-width="3" fill="red" />
-    </svg>"""
-    , unsafe_allow_html=True)
+    visacard_image()
 else:
     mastercard_image()
 
